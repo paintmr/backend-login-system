1436da7fd34044875c20987fc5a86edd87a48083
chujingxl <chujingxl@gmail.com>
2021-07-10 23:26:51 +0800

    完成了编辑和删除文章类别

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -36,7 +36,6 @@ $(function () {
       url: '/my/article/addcates',
       data: $(this).serialize(),
       success: function (res) {
-        // console.log(res);
         if (res.status !== 0) {
           return layer.msg('新增分类失败TT')
         }
@@ -47,4 +46,75 @@ $(function () {
       }
     })
   })
+
+  // 通过代理的形式，为btn-edit按钮绑定点击事件。点击编辑按钮，弹出对话框。
+  var indexEdit = null;
+  var form = layui.form
+  $('tbody').on('click', '.btn-edit', function () {
+    // 获取弹出层的索引indexAdd
+    indexEdit = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '编辑文章分类',
+      content: $('#dialog-edit').html()
+    });
+
+    var id = $(this).attr('data-id')
+    console.log(id);
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/cates/' + id,
+      success: function (res) {
+        // console.log(res);
+        form.val('form-edit', res.data)
+      }
+    })
+  })
+
+  // 通过代理的形式，为修改分类的表单绑定submit事件
+  $('body').on('submit', '#form-edit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/my/article/updatecate',
+      data: $(this).serialize(),
+      success: function (res) {
+        console.log($(this));
+        console.log(res);
+        if (res.status !== 0) {
+          return layer.msg('更新分类数据失败TT')
+        }
+        layer.msg('更新分类数据成功！')
+        // 根据索引关闭弹出层
+        layer.close(indexEdit);
+        // 更新表单数据
+        initArtCateList();
+      }
+    })
+  })
+
+  // 通过代理的形式，为删除按钮绑定点击事件
+  $('tbody').on('click', '.btn-delete', function () {
+    var id = $(this).attr('data-id')
+    // 提示用户是否要删除
+    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+      $.ajax({
+        method: 'GET',
+        url: '/my/article/deletecate/' + id,
+        success: function (res) {
+          if (res.status !== 0) {
+            return layer.msg('删除分类失败TT');
+          }
+          layer.msg('删除分类成功！')
+          // 根据索引关闭弹出层
+          layer.close(index);
+          // 更新表单数据
+          initArtCateList();
+        }
+      })
+
+      layer.close(index);
+    });
+  })
+
 })
