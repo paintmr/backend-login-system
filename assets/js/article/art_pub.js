c00f526e0ef8444ea4032c7716303bda6e49e591
chujingxl <chujingxl@gmail.com>
2021-07-19 01:34:49 +0800

    编辑文章功能完成

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -51,7 +51,6 @@ $(function () {
     }
     // 根据文件，创建对应的URL地址
     var newImgURL = URL.createObjectURL(files[0])
-    console.log(newImgURL);
     // 为裁剪区域重新设置图片
     $image
       .cropper('destroy') // 销毁旧的裁剪区域
@@ -85,8 +84,26 @@ $(function () {
         // 将文件对象存储到fd中
         fd.append('cover_img', blob)
         // console.log(blob);
-        // 发起ajax数据请求
-        publishArticle(fd)
+
+        // 看情况是新发文章还是编辑文章提交
+        if (artId) {
+          // 发起编辑文章的ajax数据请求
+          var url = '/my/article/edit'
+          var msg = {
+            success: '编辑文章成功！',
+            failure: '编辑文章失败TT'
+          }
+        } else {
+          // 发起新发文章的ajax数据请求
+          var url = '/my/article/add'
+          var msg = {
+            success: '发布文章成功！',
+            failure: '发布文章失败TT'
+          }
+        }
+        postArticle(fd, url, msg)
+
+
         // 打印看下fd里面的内容
         // fd.forEach(function (value, key) {
         //   console.log(key, value);
@@ -94,45 +111,53 @@ $(function () {
       })
   })
 
-  // 定义一个发布文章的方法
-  function publishArticle(fd) {
+  // 定义一个发布文章的方法，新发文章和编辑文章通用
+  function postArticle(fd, url, msg) {
     $.ajax({
       method: 'POST',
-      url: '/my/article/add',
+      url: url,
       data: fd,
       // 注意：如果向服务器提交的是FormData格式的数据，必须添加以下两个配置项，否则请求会失败
       contentType: false,
       processData: false,
       success: function (res) {
         if (res.status !== 0) {
-          return layer.msg('发布文章失败TT')
+          return layer.msg(msg.failure)
         }
-        layer.msg('发布文章成功！')
+        layer.msg(msg.success)
+        // 清除全局的单篇文章标记
+        artId = null;
         // 发布文章成功后，跳转到文章列表页面
         location.href = '/article/art_list.html'
         //  $('#list_art', window.parent.document)  这是找出父页面中id为list_art的元素：侧边栏“文章管理”中的“文章列表”。把“文章列表”选中，“发布文章”去掉选中样式
         $('#list_art', window.parent.document).parent().addClass('layui-this').siblings().removeClass('layui-this')
-
       }
     })
   }
 
   // 如果是从art_list.js点击了编辑按钮跳转过来的
-  if (window.parent.artId) {
+  var artId = window.parent.artId
+  window.parent.artId = null
+  if (artId) {
     $.ajax({
       method: 'GET',
-      url: '/my/article/' + window.parent.artId,
+      url: '/my/article/' + artId,
       success: function (res) {
-        console.log(res.data);
-        var artCateId = res.data.cate_id
+        // console.log(res.data);
+
         // 根据cate_id获取文章分类名称
+        var artCateId = res.data.cate_id
         $.ajax({
           method: 'GET',
           url: '/my/article/cates/' + artCateId,
           success: function (res) {
-            // console.log(res);
-            $('#artCateDe').html(res.data.name);
-            // console.log($('#artCateDe'));
+            artCateId = artCateId + '';
+            for (let i = 0; i < $('#artCate')[0].length; i++) {
+              // console.log(($('#artCate')[0].options[i].value));打印出来是string，所以要把上面的artCateId转换为string才好比较
+              if (($('#artCate')[0].options[i].value) === artCateId) {
+                $('#artCate')[0].options[i].selected = true
+              }
+            }
             form.render();
           }
         })
@@ -146,11 +171,11 @@ $(function () {
           .cropper(options) // 重新初始化裁剪区域
 
         // 把其它数据放入form表单中
-        // form.val('form-pub', res.data) //这种方式无法把数据放入form中
-        form.val('form-pub', JSON.parse(JSON.stringify(res.data)))
+        form.val('form-pub', res.data) //如果这种方式无法把数据放入form中，尝试下面的方法
+        // form.val('form-pub', JSON.parse(JSON.stringify(res.data)))
       }
     })
-    window.parent.artId = null;
   }
+
 
 })
